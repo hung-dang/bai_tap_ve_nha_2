36702d7279d1b7d09da742375a4f970fecb0c987
hung-dang <61694320+hung-dang@users.noreply.github.com>
2020-03-08 21:19:21 +0700

    ok

---
+++ src/com/trungtamjava/model/KeToan.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.trungtamjava.model;
+
+/**
+ *
+ * @author Admin
+ */
+public class KeToan {
+
+    public static int luongKeToan(int soGio) {
+        return soGio * 2000000;
+    }
+
+}
---
+++ src/com/trungtamjava/model/LeTan.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.trungtamjava.model;
+
+/**
+ *
+ * @author Admin
+ */
+public class LeTan {
+
+    public static int luongLeTan(int soGio) {
+        return soGio * 100000;
+    }
+}
